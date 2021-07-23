0f9c8a3f302c2e56a018660ab44a67a4c7ae9ad7
guilhermevieira08 <63257913+guilhermevieira08@users.noreply.github.com>
2021-07-23 10:11:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,119 @@
+//Initial Data
+let square = {
+    a1:'',a2:'',a3:'',
+    b1:'',b2:'',b3:'',
+    c1:'',c2:'',c3:''
+};
+let player = '';
+let warning = '';
+let playing = false;
+
+reset();
+//Events
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach(item => {
+    item.addEventListener('click', itemClick)
+})
+
+//Functions
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    if(playing && square[item] === ''){
+        square[item] = player;
+        renderSquare();
+        tooglePlayer();
+    }
+}
+/**
+ * Function reset table
+ * 
+ * @example 
+ *   undefined
+ * 
+ * @param   {Number} obrigatório   Parametro obrigatório
+ * @param   {Number} [opcional]    Parametro ocional. Note os '[ ]'
+ * @returns nothing
+ */
+function reset() {
+    warning = '';
+
+    let random = Math.floor(Math.random() * 2);
+    player = random === 0 ? 'x' : 'o';
+
+    //clean table
+    for(let i in square) {
+        square[i] = ''
+    }
+
+    //reset game
+    playing = true;
+
+    renderSquare();
+    renderInfo();
+}
+
+function renderSquare() {
+    for(let i in square) {
+        let item = document.querySelector(`div[data-item=${i}]`)
+        item.innerHTML = square[i]
+    }
+
+    checkGame();
+}
+
+function renderInfo() {
+    document.querySelector('.vez').innerHTML = player;
+    document.querySelector('.resultado').innerHTML = warning;
+}
+
+function tooglePlayer() {
+    player = player === 'x' ? 'o' : 'x'; 
+    renderInfo();
+}
+
+function checkGame() {
+    if(checkWinnerFor('x')) {
+        warning = 'O "x" Venceu';
+        playing = false;
+    } else if(checkWinnerFor('o')) {
+        warning = 'O "o" venceu';
+        playing = false;
+    } else if(isFull()) {
+        warning = 'Deu Empate';
+        playing = false;
+    }
+}
+
+function checkWinnerFor(player) {
+    let possibilities = [
+        'a1,a2,a3',
+        'b1,b2,b3',
+        'c1,c2,c3',
+
+        'a1,b1,c1',
+        'a2,b2,c2',
+        'a3,b3,c3',
+
+        'a1,b2,c3',
+        'a3,b2,c1',
+    ];
+
+    for(let w in possibilities) {
+        let pArray = possibilities[w].split(',');
+        let hasWon = pArray.every(option => square[option] === player);
+        if(hasWon) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isFull() {
+    for(let i in square) {
+        if(square[i] === '') {
+            return false;
+        }
+    }
+
+    return true;
+}
